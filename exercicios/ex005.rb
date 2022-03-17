b321e8360be4a143a189ec5fdd735ac59a0d48f0
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-17 13:11:02 -0400

    Update ex005.rb
    solução usando o método .select

--- exercicios/ex005.rb
+++ exercicios/ex005.rb
@@ -1,11 +1,7 @@
 list = ['a', 'sw', 'f', 'y', 'xF', 'j', 'xF']
 
 def index_of(list, value)
-  posicoes = []
-  list.each_with_index do |item, indice|
-    posicoes << indice if item.include? value
-  end
-  return posicoes
+  puts list.each_index.select{|valor| list[valor].include? value}
 end
 
 index_of(list, 'F')
