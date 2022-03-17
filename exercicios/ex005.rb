535fabe3cf017a084227d176a576459d7f796c13
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-17 12:44:43 -0400

    Update ex005.rb

--- exercicios/ex005.rb
+++ exercicios/ex005.rb
@@ -1,7 +1,11 @@
-list = ['a', 'sw', 'f', 'y', 'xF', 'j']
+list = ['a', 'sw', 'f', 'y', 'xF', 'j', 'xF']
 
 def index_of(list, value)
-  list.select{ |valor| valor.end_with? value }
+  posicoes = []
+  list.each_with_index do |item, indice|
+    posicoes << indice if item.include? value
+  end
+  return posicoes
 end
 
 index_of(list, 'F')
