ae00fe14fe40dc04f07daeeab5054024425de1ef
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-19 15:15:34 -0400

    Update ex007.rb

--- exercicios/ex007.rb
+++ exercicios/ex007.rb
@@ -7,14 +7,19 @@ def self.multiplos(qtd, multiplo)
 end
 
 def self.tabuada(fim_tabuada)
+  tabuada = []
+  inicio=1
   while inicio <= fim_tabuada
+    multiplos = []
     for x in (1..10)
-      tabuada << inicio * x 
+      multiplos << inicio * x
     end
+    tabuada << multiplos  
     inicio += 1
   end
+  puts tabuada
 end
 
-multiplos(5,2)
+#multiplos(5,2)
 tabuada(10)
 
