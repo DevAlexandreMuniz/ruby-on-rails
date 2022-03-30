68e5a4694f38ab4b9da0602fa44fc3a8bcbf2290
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-29 22:02:55 -0400

    Create ex008.rb

---
+++ exercicios/ex008.rb
@@ -0,0 +1,11 @@
+array = [1, 2, 3, 4, 5, 6]
+
+def self.multiplica_antecessor_predecessor(array)
+  resultado = []
+  
+  array.with_index do |value, index|
+    
+  end
+end
+
+multiplica_antecessor_predecessor(array)
